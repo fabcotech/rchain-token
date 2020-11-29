282c2e5ec3be7bdd4ace900c505869e6c3511e3d
raf <raf@raf.com>
2020-11-29 18:32:06 +0100

    - added compilation for the browser

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    format: 'umd',
+    name: 'RChainToken',
+    file: 'dist/rchain-token.js',
+  },
+  plugins: [resolve(), commonjs(), json()],
+};
