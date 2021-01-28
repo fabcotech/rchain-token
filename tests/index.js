a0aec91926f7af18d8d4c4ce4b2f4ce440e4465b
raf <raf@raf.com>
2021-01-28 14:39:31 +0100

    added dust cost at each step of test

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 const rc = require('rchain-toolkit');
 require('dotenv').config();
 
+const getBalance = require('./getBalance').main;
 const getAllData = require('./getAllData').main;
 const deploy = require('./01_deploy').main;
 const checkBagsAndTokens = require('./02_checkBagsAndTokens.js').main;
@@ -13,7 +14,7 @@ const checkBagData = require('./08_checkBagData.js').main;
 const sendTokens = require('./09_sendTokens.js').main;
 const checkBagsAndTokens4 = require('./10_checkBagsAndTokens.js').main;
 
-const BAGS_TO_CREATE = 1000;
+const BAGS_TO_CREATE = 100;
 
 const PRIVATE_KEY =
   '28a5c9ac133b4449ca38e9bdf7cacdce31079ef6b3ac2f0a080af83ecff98b36';
@@ -23,9 +24,19 @@ const PRIVATE_KEY_2 =
   'a2803d16030f83757a5043e5c0e28573685f6d8bf4e358bf1385d82bffa8e698';
 const PUBLIC_KEY_2 = rc.utils.publicKeyFromPrivateKey(PRIVATE_KEY_2);
 
+const balances1 = [];
+const balances2 = [];
+
 const main = async () => {
+  balances1.push(await getBalance(PUBLIC_KEY));
+  balances2.push(await getBalance(PUBLIC_KEY_2));
   const data = await deploy(PRIVATE_KEY, PUBLIC_KEY);
+  balances1.push(await getBalance(PUBLIC_KEY));
   console.log('✓ 01 deploy');
+  console.log(
+    '  01 dust cost: ' +
+      (balances1[balances1.length - 2] - balances1[balances1.length - 1])
+  );
   await checkBagsAndTokens(data.registryUri.replace('rho:id:', ''));
   console.log('✓ 02 check initial bags and data');
   await createTokens(
@@ -35,7 +46,12 @@ const main = async () => {
     data.nonce,
     BAGS_TO_CREATE
   );
+  balances1.push(await getBalance(PUBLIC_KEY));
   console.log(`✓ 03 create ${BAGS_TO_CREATE} bags`);
+  console.log(
+    '  03 dust cost: ' +
+      (balances1[balances1.length - 2] - balances1[balances1.length - 1])
+  );
   await checkBagsAndTokens2(
     data.registryUri.replace('rho:id:', ''),
     BAGS_TO_CREATE
@@ -46,21 +62,31 @@ const main = async () => {
     PRIVATE_KEY_2,
     PUBLIC_KEY_2
   );
+  balances2.push(await getBalance(PUBLIC_KEY_2));
   console.log('✓ 05 purchase 1 token from bag 1');
+  console.log(
+    '  05 dust cost: ' +
+      (balances2[balances2.length - 2] - balances2[balances2.length - 1])
+  );
   const lastBag = await checkBagsAndTokens3(
     data.registryUri.replace('rho:id:', ''),
     BAGS_TO_CREATE,
     PUBLIC_KEY_2
   );
   console.log(`✓ 06 check the presence of ${BAGS_TO_CREATE + 1} bags`);
-  const newNonce = await updateBagData(
+  await updateBagData(
     data.registryUri.replace('rho:id:', ''),
     lastBag.nonce,
     BAGS_TO_CREATE,
     PRIVATE_KEY_2,
     PUBLIC_KEY_2
   );
+  balances2.push(await getBalance(PUBLIC_KEY_2));
   console.log(`✓ 07 update data associated with bag ${BAGS_TO_CREATE}`);
+  console.log(
+    '  07 dust cost: ' +
+      (balances2[balances2.length - 2] - balances2[balances2.length - 1])
+  );
   await checkBagData(data.registryUri.replace('rho:id:', ''), BAGS_TO_CREATE);
   console.log(`✓ 08 check data associated with bag ${BAGS_TO_CREATE}`);
   const allData = await getAllData(data.registryUri.replace('rho:id:', ''));
@@ -70,7 +96,12 @@ const main = async () => {
     PRIVATE_KEY,
     PUBLIC_KEY
   );
+  balances1.push(await getBalance(PUBLIC_KEY));
   console.log(`✓ 09 send 1 token from bag 1`);
+  console.log(
+    '  09 dust cost: ' +
+      (balances1[balances1.length - 2] - balances1[balances1.length - 1])
+  );
   await checkBagsAndTokens4(
     data.registryUri.replace('rho:id:', ''),
     BAGS_TO_CREATE
